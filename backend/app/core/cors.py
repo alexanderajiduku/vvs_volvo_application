284e7e7295be3a4aba0d79a09a40e68338817eec
alexanderajiduku <alexajiduku@gmail.com>
2024-02-27 22:22:20 +0100

    addition

--- backend/app/core/cors.py
+++ backend/app/core/cors.py
@@ -1,17 +1,24 @@
-# app/config/cors.py
-from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import logging
 
+# Setup logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Existing CORS setup
 def setup_cors(app):
     origins = [
         "https://vvs-volvo-application-frontend.onrender.com",
-        "http://localhost:3000"
+        "http://localhost:3000",
     ]
 
     app.add_middleware(
         CORSMiddleware,
         allow_origins=origins,
         allow_credentials=True,
-        allow_methods=["*"], 
-        allow_headers=["*"],  
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
+
+
+
